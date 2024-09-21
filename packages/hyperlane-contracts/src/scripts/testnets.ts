1b66232a24ef9f78a816910dae3c4ea4f9d92c98
Oscar Baracos <oscar.baracos@gmail.com>
2024-09-21 23:22:37 +0800

    Update testnet script

--- packages/hyperlane-contracts/src/scripts/testnets.ts
+++ packages/hyperlane-contracts/src/scripts/testnets.ts
@@ -1,6 +1,6 @@
 import { Address, createPublicClient, createWalletClient, encodeFunctionData, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { bscTestnet, plumeTestnet } from "viem/chains";
+import { scrollSepolia, bscTestnet } from "viem/chains";
 import { GithubRegistry } from "@hyperlane-xyz/registry";
 import {
     encodeCallRemoteWithOverrides,
@@ -10,6 +10,10 @@ import {
 } from "../InterchainAccountRouter.js";
 import { defaultIsm as defaultIsmAbi } from "../artifacts/IMailbox.js";
 import { domains } from "../artifacts/IRouter.js";
+import "dotenv/config";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+if (!PRIVATE_KEY) throw new Error("Need PRIVATE_KEY");
 
 interface InterchainAddresses {
     mailbox: Address;
@@ -17,33 +21,34 @@ interface InterchainAddresses {
     interchainAccountRouter: Address;
 }
 
+const scrollSepoliaClient = createPublicClient({
+    chain: scrollSepolia,
+    transport: http(),
+});
+
 const bscTestnetClient = createPublicClient({
     chain: bscTestnet,
     transport: http(),
 });
 
-const plumeTestnetClient = createPublicClient({
-    chain: plumeTestnet,
-    transport: http(),
-});
-
-const bscTestnetWalletClient = createWalletClient({
-    chain: bscTestnet,
+const scrollSepoliaWalletClient = createWalletClient({
+    chain: scrollSepolia,
     transport: http(),
     //anvil
     account: privateKeyToAccount("0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e"),
 });
 
 const PROXY_DEPLOYED_URL = "https://proxy.hyperlane.xyz";
-export async function getInterchainChainAddresses(chainName: string) {
-    const registry = new GithubRegistry({
-        proxyUrl: PROXY_DEPLOYED_URL,
-    });
+const registry = new GithubRegistry({
+    proxyUrl: PROXY_DEPLOYED_URL,
+});
 
+export async function getInterchainChainAddresses(chainName: string) {
     const addresses = await registry.getChainAddresses(chainName);
     if (!addresses) throw new Error(`${chainName} addresses not found`);
 
-    if (!addresses["mailbox"] || addresses["interchainAccountIsm"] || addresses["interchainAccountRouter"]) {
+    if (!addresses["mailbox"] || !addresses["interchainAccountIsm"] || !addresses["interchainAccountRouter"]) {
+        console.log({ addresses });
         throw new Error(`${chainName} not supported`);
     }
 
@@ -51,24 +56,24 @@ export async function getInterchainChainAddresses(chainName: string) {
 }
 
 async function main() {
+    const scrollSepoliaAddresses = await getInterchainChainAddresses("scrollsepolia");
     const bscTestnetAddresses = await getInterchainChainAddresses("bsctestnet");
-    const plumeTestnetAddresses = await getInterchainChainAddresses("plumetestnet");
 
     // Get the interchain account address from the main chain using its owner
     const interchainAccountFromMain = await getRemoteInterchainAccount({
-        publicClient: bscTestnetClient,
-        remoteRouter: plumeTestnetAddresses.interchainAccountRouter,
-        remoteIsm: plumeTestnetAddresses.interchainAccountIsm,
-        mainRouter: bscTestnetAddresses.interchainAccountRouter,
-        owner: bscTestnetWalletClient.account.address,
+        publicClient: scrollSepoliaClient,
+        remoteRouter: bscTestnetAddresses.interchainAccountRouter,
+        remoteIsm: bscTestnetAddresses.interchainAccountIsm,
+        mainRouter: scrollSepoliaAddresses.interchainAccountRouter,
+        owner: scrollSepoliaWalletClient.account.address,
     });
 
     // Get interchain account address from the remote chain using the owner & origin
     const interchainAccountFromRemote = await getLocalInterchainAccount({
-        publicClient: plumeTestnetClient,
-        origin: bscTestnet.id,
+        publicClient: bscTestnetClient,
+        origin: scrollSepolia.id,
         router: bscTestnetAddresses.interchainAccountRouter,
-        owner: bscTestnetWalletClient.account.address,
+        owner: scrollSepoliaWalletClient.account.address,
         ism: bscTestnetAddresses.interchainAccountIsm,
     });
 
@@ -76,8 +81,8 @@ async function main() {
     console.debug({ interchainAccountFromMain, interchainAccountFromRemote });
 
     // get default ISM
-    const defaultIsm = await plumeTestnetClient.readContract({
-        address: plumeTestnetAddresses.mailbox,
+    const defaultIsm = await bscTestnetClient.readContract({
+        address: bscTestnetAddresses.mailbox,
         abi: [defaultIsmAbi],
         functionName: "defaultIsm",
     });
@@ -85,7 +90,7 @@ async function main() {
     // we test out a simple read call from the interchain account
     const calls = [
         {
-            to: plumeTestnetAddresses.interchainAccountRouter,
+            to: bscTestnetAddresses.interchainAccountRouter,
             data: encodeFunctionData({
                 abi: [domains],
                 functionName: "domains",
@@ -95,31 +100,37 @@ async function main() {
 
     // call remote data
     const callRemoteData = encodeCallRemoteWithOverrides({
-        destination: plumeTestnet.id,
-        router: bscTestnetAddresses.interchainAccountRouter,
+        destination: bscTestnet.id,
+        router: scrollSepoliaAddresses.interchainAccountRouter,
         ism: defaultIsm,
         calls,
     });
 
+    const remoteGasEstimate = await bscTestnetClient.estimateGas(calls[0]);
+
+    console.log({ remoteGasEstimate });
+
     const gasQuote = await quoteGasPayment({
-        publicClient: bscTestnetClient,
-        destination: plumeTestnet.id,
-        gasLimit: 100000000000000n,
-        router: bscTestnetAddresses.interchainAccountRouter,
+        publicClient: scrollSepoliaClient,
+        destination: bscTestnet.id,
+        gasLimit: remoteGasEstimate,
+        router: scrollSepoliaAddresses.interchainAccountRouter,
     });
 
-    // 130%
-    const gasQuoteOverestimate = (gasQuote * 130n) / 10n;
+    console.log({ gasQuote });
 
-    //execute
-    const hash = await bscTestnetWalletClient.sendTransaction({
-        to: bscTestnetAddresses.interchainAccountRouter,
+    // 130%
+    const gasQuoteOverestimate = (gasQuote * 13n) / 10n;
+    //
+    // //execute
+    const hash = await scrollSepoliaWalletClient.sendTransaction({
+        to: scrollSepoliaAddresses.interchainAccountRouter,
         data: callRemoteData,
         value: gasQuoteOverestimate,
     });
 
     console.debug({ hash });
-    const receipt = await bscTestnetClient.waitForTransactionReceipt({ hash });
+    const receipt = await scrollSepoliaClient.waitForTransactionReceipt({ hash });
     console.debug({ receipt });
 }
 
